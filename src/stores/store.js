4bfa2f9fc2e05fbac850a49920df8780f218e10c
mcasimir <maurizio.cas@gmail.com>
2019-12-17 16:40:00 +0100

    feat: remove legacy code and exports

--- src/stores/store.js
+++ src/stores/store.js
@@ -7,35 +7,6 @@ const metricsStore = createStore(reducer);
 const metrics = require('mongodb-js-metrics')();
 
 /**
- * Legacy tracking for Reflux store updates.
- *
- * @param {AppRegistry} appRegistry - The app registry.
- * @param {String} storeName - The store name.
- * @param {Object} rule - The rule.
- * @param {String} version - The compass version.
- */
-const trackStoreUpdate = (appRegistry, storeName, rule, version) => {
-  const store = appRegistry.getStore(storeName);
-  if (!store) {
-    return;
-  }
-  // attach an event listener
-  store.listen(async(state) => {
-    // only track an event if the rule condition evaluates to true
-    if (rule.condition(state)) {
-      // Some stores trigger with arrays of data.
-      if (rule.multi) {
-        for (const singleState of state) {
-          metrics.track(rule.resource, rule.action, await rule.metadata(version, singleState));
-        }
-      } else {
-        metrics.track(rule.resource, rule.action, await rule.metadata(version, state));
-      }
-    }
-  });
-};
-
-/**
  * Tracking of events that are emitted on the app registry.
  *
  * @param {AppRegistry} appRegistry - The app registry.
@@ -64,18 +35,10 @@ metricsStore.onActivated = (appRegistry) => {
 
     // configure rules
     rules.forEach((rule) => {
-      // get the store for this rule
-      const storeName = rule.store;
       const eventName = rule.registryEvent;
-
-      if (storeName) {
-        trackStoreUpdate(appRegistry, storeName, rule, version);
-      } else if (eventName) {
-        trackRegistryEvent(appRegistry, eventName, rule, version);
-      }
+      trackRegistryEvent(appRegistry, eventName, rule, version);
     });
   });
 };
 
 export default metricsStore;
-export { trackStoreUpdate, trackRegistryEvent };
